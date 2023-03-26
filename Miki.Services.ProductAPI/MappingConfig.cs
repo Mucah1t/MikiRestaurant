455be08964ef810088a0d5358bf324fd4d2b628e
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-03-26 17:43:07 +0300

    Add project files.

---
+++ Miki.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Miki.Services.ProductAPI.Models;
+using Miki.Services.ProductAPI.Models.DTO;
+
+namespace Miki.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>(); //.reverse method can be used for reverse method instead of writing 
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ Miki.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Miki.Services.ProductAPI.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace Miki.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
